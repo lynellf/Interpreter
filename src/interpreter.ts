95a70bb5f956996ea760ef1ac0bb5c1f5eb21bd9
Ezell <lynellf@gmail.com>
2020-02-29 14:36:50 -0600

    added support for side-effects

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,4 +1,9 @@
-type TState<S> = Record<string, S>;
+type TSideEffect<S> = (
+  state: TState<S>,
+  ...args: unknown[]
+) => TState<S> | void;
+type TSideEffects<S> = TSideEffect<S>[];
+type TState<S> = Record<string, TSideEffects<S> | S>;
 type TAction<V, S> = (variables: readonly V[], state: TState<S>) => TState<S>;
 type TConstraint<S> = (state: TState<S>) => boolean;
 type TDescription<V, S> = {
@@ -11,35 +16,46 @@ type TDescription<V, S> = {
 export default class Interpreter<V, S> {
   variables: V[] = [];
   actions: TAction<V, S>[] = [];
-  state: TState<S>;
+  state: TState<S> = { sideEffects: [] };
   constraints: TConstraint<S>[] = [];
 
   constructor(description: TDescription<V, S>) {
     const { variables, actions, initialState, constraints } = description;
     this.variables = variables;
     this.actions = actions;
-    this.state = initialState;
+    this.state = { ...this.state, ...initialState };
     this.constraints = constraints;
   }
-
-  private callFunc = (func: TAction<V, S>) => {
-    const state = Object.freeze({ ...this.state });
-    const variables = Object.freeze([...this.variables]);
-    this.state = func(variables, state);
-  };
 
   private assert = (state: TState<S>, objective: TConstraint<S>) => {
     return objective(state);
   };
 
+  private callEffect = (state: TState<S>, effect: TSideEffect<S>) => {
+    const out = effect(state) ? effect(state) : state;
+    return out as TState<S>;
+  };
+
+  private callAction = (state: TState<S>, action: TAction<V, S>) => {
+    const callEffect = this.callEffect;
+    const variables = Object.freeze(this.variables);
+    const outState = action(variables, state);
+    const sideEffects = outState.sideEffects as TSideEffects<S>;
+    const afterEffects = sideEffects.reduce(
+      (s, eff) => callEffect(s, eff),
+      outState
+    );
+    return afterEffects;
+  };
+
   interpret = () => {
-    const { callFunc, assert } = this;
+    const { assert, callAction } = this;
+    const state = Object.freeze(this.state);
     const actions = this.actions;
     const constraints = this.constraints;
-    actions.forEach(func => callFunc(func));
-    const state = this.state;
-    const assertions = constraints.map(obj => assert(state, obj));
+    const output = actions.reduce((s, a) => callAction(s, a), state);
+    const assertions = constraints.map(obj => assert(output, obj));
 
-    return [state, assertions] as const;
+    return [output, assertions] as const;
   };
 }
